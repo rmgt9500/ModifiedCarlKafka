aa0627f41a70457172286d465bcd9675a8604d7c
rmgt9500 <155571898+rmgt9500@users.noreply.github.com>
2026-02-09 15:45:30 +0800

    Initiate

---
+++ kafka/consumer/src/main/java/com/gabriel/pss/Main.java
@@ -0,0 +1,14 @@
+package com.gabriel.pss;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author carl emmanuel macabales
+ */
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Main.class, args);
+    }
+}
